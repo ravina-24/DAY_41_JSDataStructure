2f21f59f8fb8e014a7b651bae710f1e9f3e64169
ravina-24 <ravinamaudekar24@gmail.com>
2021-08-24 23:56:34 +0530

    [Ravina] Added: Finding the second largest and smallest number inan array using sort function.

---
+++ ArrayPracticeProblem/FindSecondLargestAndSmallestNumber.js
@@ -0,0 +1,14 @@
+let maxcount = 10;
+let count =1;
+let ArrayofNum = [];
+while (count <=maxcount)
+{
+  let number = (Math.floor(Math.random()*900)+99);
+  ArrayofNum.push(number);
+  console.log(ArrayofNum);
+  count++;
+}
+
+ArrayofNum.sort();
+console.log("The second largest element is =" +  ArrayofNum[1]);
+console.log("The second smallest element is =" + ArrayofNum[8]);
